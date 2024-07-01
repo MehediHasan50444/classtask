7ddef00ce5722876544ad24a5aaa741868d4dd9b
MehediHasan50444 <mmhmp40@gmail.com>
2024-07-01 12:49:33 +0600

    Add files via upload

---
+++ stracknormal.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#include<stack>
+using namespace std;
+
+int main(){
+stack<int> s;
+
+s.push(10);
+
+s.push(20);
+
+s.push(30);
+
+s.push(40);
+
+
+cout<<s.top();
+s.pop();
+cout<<s.top();
+
+
+}
